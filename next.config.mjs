0898413ce944265c9506f55dc4b77dd0f94b7810
mucahidkaya <71714025+mucahidkaya@users.noreply.github.com>
2025-04-25 10:16:55 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export', // Enable static export in Next.js 13+ and 15
   trailingSlash: true, // Helps GitHub Pages routing
+  basePath: '/namaz', // <== set this to your GitHub repo name
+  assetPrefix: '/namaz' // <== also set this for media/scripts/css
 };
 
 export default nextConfig;
